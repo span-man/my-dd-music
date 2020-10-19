bec520699d01344f9a46342d4d3187475d2d5a6a
spanman <1226497103@qq,com>
2020-10-20 00:08:43 +0800

    1020

--- src/components/C030bottom/index.js
+++ src/components/C030bottom/index.js
@@ -8,14 +8,35 @@ import Axios from "axios";
 import { StepBackwardOutlined, StarFilled, PlayCircleOutlined, StepForwardOutlined, PauseCircleOutlined } from '@ant-design/icons';
 export default class C030bottom extends React.Component {
     dom;
+    aaaabb;
+    audioDom;
+    playNodeDom;
+    hadPlayed;
+    diffX;
+    playNodeBoxCW;
+    diameterC2;
+
+    processLineDom; // 整个进度条盒子
+    bufferLineDom; // 缓冲进度条
+    playNodeBoxDom; // 放已经播放 和 滑块的槽子
     constructor(props) {
         super(props)
         this.state = {
             dataList: [], // 所有歌曲的List
             playBool: false, // 播放与否 (而且可以控制初始是否播放)
             nowIndex: 0, // 当前播放的下标
-
-            musicName: "-- --",
+            
+            /**
+             * 开始时候获取该首歌的 名字 时长 
+             * 每隔500ms获取 当前缓存的长度 播放进度
+             */
+            musicInfo: {
+                duration: "", // 时长 00:00 形式
+                musicName: "-- --", // 歌曲名字
+                musicLength: 0, // 歌曲时长 秒数
+                bufferedPercent: 0, // 缓冲进度条 百分比
+                playedTime: 0 // 播放进度 时长
+            },
 
             buttonStyle: { fontSize: '24px', color: '#32c3de' },
             buttonCenterStyle: { fontSize: '30px', color: '#32c3de' }
@@ -27,16 +48,16 @@ export default class C030bottom extends React.Component {
             <div className="c-bottom-content-box dd-column">
                 {/* 进度条 */}
                 <div className="process-line dd-row dd-v-center">
-                    {/* 游标的盒子 右侧少占游标宽度的一半 */}
-                    <div className="play-node-box dd-row dd-v-center">
-                        {/* 播放游标 */}
-                        <div className="play-node" draggable="true" onDrag={(e) => this.handelDrag(e)} onDragEnd={(e) => this.onDragEnd(e)}>
-                        </div>
-                    </div>
                     {/* 缓存进度条 */}
                     <div className="cache-line ">
-                        {/* 已经播放的 */}
-                        <div className="had-played dd-row dd-v-center">
+                    </div>
+                    {/* 滑块的盒子 右侧少占滑块宽度的一半 */}
+                    <div className="play-node-box dd-row dd-v-center">
+                        {/* 播放进度条 */}
+                        <div className="had-played">
+                        </div>
+                        {/* 播放滑块 */}
+                        <div className="play-node" draggable="true" onDragStart={(e) => this.handelDragStart(e)} onDrag={(e) => this.handelDrag(e)} onDragEnd={(e) => this.handelDragEnd(e)}>
                         </div>
                     </div>
                 </div>
@@ -56,13 +77,18 @@ export default class C030bottom extends React.Component {
                     <div className="c-bottom-content-center">
                         {/* 歌名和时间 */}
                         <div className="name-time">
-                            <span>{this.state.musicName}</span>
-                            <span>00:00 / 04:13</span>
+                            <span>{this.state.musicInfo.musicName}</span>
+                            <span>{this.state.musicInfo.playedTime} / {this.state.musicInfo.duration}</span>
                         </div>
                         {/* 进度条 */}
                         <div>
                             {/* 进度条 */}
-                            <audio id="mydom" preload="auto" src={mp31} onEnded={() => this.musicEnd()}>
+                            <audio id="mydom" preload="auto" src={mp31}
+                                onPlaying={() => this.musicPlaying()}
+                                onTimeUpdate={() => this.musicTimeUpdate()}
+                                onPause={() => this.musicPause()}
+                                onEnded={() => this.musicEnd()}
+                            >
                             </audio>
                         </div>
                     </div>
@@ -76,6 +102,15 @@ export default class C030bottom extends React.Component {
     componentDidMount() {
         this.getAllData();
         this.dom = document.getElementById('mydom');
+        this.audioDom = document.querySelector('#mydom');
+
+        // 整个进度条盒子
+        this.processLineDom = document.querySelector('.process-line');
+        // 缓冲进度条
+        this.bufferLineDom = document.querySelector('.cache-line');
+        // 播放进度条
+        this.playNodeBoxDom = document.querySelector(".play-node-box")
+
         this.playOrStop()
 
         store.subscribe(() => {
@@ -84,6 +119,8 @@ export default class C030bottom extends React.Component {
                 this.changeMusic(temp.value)
             }
         })
+
+        this.aaaabb = common.throttle(this.makeDataINeed, 1000, 1)
     }
 
     changeMusic(obj) {
@@ -129,8 +166,11 @@ export default class C030bottom extends React.Component {
 
     // 播放具体某歌曲
     myPlay(urlStr) {
+        let _musicInfo = this.state.musicInfo
+        let arr = urlStr.split('/');
+        _musicInfo.musicName = arr[arr.length - 1]
         this.setState({
-            musicName: urlStr
+            musicInfo: _musicInfo
         })
         this.dom.src = common.getDomain() + urlStr
         this.dom.play()
@@ -162,21 +202,126 @@ export default class C030bottom extends React.Component {
         this.myPlay(this.state.dataList[this.state.nowIndex])
     }
 
+    // 播放开始 播放开始的时候只会触发一次
+    musicPlaying(e) {
+        this.playNodeDom = document.querySelector(".play-node");
+
+        this.playNodeBoxCW = document.querySelector(".play-node-box").clientWidth;
+        this.diameterC2 = document.querySelector(".play-node").clientWidth / 2;
+        this.hadPlayed = document.querySelector(".had-played");
+        //播放状态Doing
+        // playOrPause.src = require('../assets/img/pause.svg')//背景音乐是播放还是暂停
+        // _this.playState = true;//播放状态
+        console.log("播放中开始")
+        console.log("C030bottom -> componentDidMount -> 音频长度 this.dom.duration", this.audioDom.duration)
+        let tempObj = this.state.musicInfo
+        tempObj.musicLength = (this.audioDom.duration)
+        tempObj.duration = common.makeMinutes(this.audioDom.duration)
+        this.setState({
+            musicInfo: tempObj
+        })
+    }
+
+    // 缓冲进度条 已经播放进度条
+    makeDataINeed() {
+        console.log("播放中 每隔一秒 获取一次");
+        // 获取已经缓存的时间
+        const timeRanges = this.dom.buffered;
+        if (timeRanges.length != 0) {
+            let tempTimeRangesNum = timeRanges.end(timeRanges.length - 1);
+
+            // 计算百分比 展示缓冲进度条
+            let a = parseInt(tempTimeRangesNum * 100 / this.state.musicInfo.musicLength * 100) / 100
+            console.log("C030bottom -> makeDataINeed -> a", a)
+            let tempObj = this.state.musicInfo
+            tempObj.bufferedPercent = a
+            this.setState({
+                musicInfo: tempObj
+            })
+
+            // 改变缓冲进度条
+            this.changeBuffer(a)
+            // 改变播放进度
+            this.changePlayNode()
+        }
+    }
+
+    // 播放中
+    musicTimeUpdate(e) {
+        // console.log("播放中 函数默认的调取");
+        this.aaaabb();
+    }
+
+    // 播放暂停
+    musicPause() {
+        console.log("播放暂停")
+    }
+
     // 播放结束
     musicEnd() {
-        this.nextOne()
+        console.log("停止了");
+        this.nextOne();
     }
 
+    // 判断位置
+    judgePosition(e) {
+        // 滑块的滑轨
+        let tempWdith = this.playNodeBoxCW - this.diameterC2
+        let tempNum = 0
+        if (e.clientX <= this.diameterC2) {
+            tempNum = 0
+        }
+        else if (e.clientX >= tempWdith) {
+            tempNum = tempWdith
+        }
+        else {
+            tempNum = e.clientX - this.diffX
+        }
+        this.playNodeDom.style.left = tempNum + 'px'
+        this.hadPlayed.style.width = tempNum + 'px'
+    }
+    // 拖拽 开始
+    handelDragStart(e) {
+        this.diffX = e.clientX - this.playNodeDom.offsetLeft;
+    }
     // 拖拽
     handelDrag(e) {
-        console.log("拖拽中")
-        document.querySelector(".had-played").style.width = e.clientX + 'px'
+        this.judgePosition(e)
     }
-    // 拖拽
-    onDragEnd(e) {
-        console.log("结束")
-        document.querySelector(".had-played").style.width = e.clientX + 'px'
+    // 拖拽 结束
+    handelDragEnd(e) {
+        this.judgePosition(e)
     }
 
+    // 改变缓冲进度条
+    changeBuffer(percent) {
+        if (percent > 100) {
+            return
+        }
+        let bufferWidth = this.processLineDom.clientWidth * percent / 100
+        this.bufferLineDom.style.width = bufferWidth + 'px'
+    }
 
-} 
+    // 改变播放进度
+    changePlayNode() {
+        console.log("当前时间-->", this.dom.currentTime)
+        let timeDisplay = this.dom.currentTime;//获取实时时间
+        // 百分比
+        let tempPercent = timeDisplay / this.state.musicInfo.musicLength
+        console.log("已经播放的百分比是 -> tempPercent", tempPercent)
+        let tempNum = tempPercent * this.playNodeBoxDom.clientWidth
+        this.playNodeDom.style.left = tempNum + 'px'
+        this.hadPlayed.style.width = tempNum + 'px'
+
+        let tempObj = this.state.musicInfo
+        tempObj.playedTime = common.makeMinutes(timeDisplay)
+        this.setState({
+            musicInfo: tempObj
+        })
+
+    }
+}
+
+
+// 隐藏拖拽
+// dataTransfer.setDragImage() 方法可以设置拖拽时的图片，默认就是一个半透明的内容
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,9 +1,53 @@
 export default {
     getDomain() {
-        // if(window.location.host.includes("derlol.top")) {
+        // if (window.location.host.includes("derlol.top")) {
         return "http://derlol.top:3002"
         // } else {
         //     return "http://localhost:3002"
         // }
+    },
+    // 补全前面
+    myPadStart(_num, _num2) {
+        let str = String(_num)
+        return str.padStart(_num2, "0")
+    },
+    // 将秒转成分钟
+    makeMinutes(_seconds) {
+        if (!_seconds) {
+            return "00:00"
+        }
+        let tempStr = "";
+        let t = Number(_seconds).toFixed(0)
+        console.log("makeMinutes -> t", t)
+        let minutes = Math.floor(t / 60)
+        let second = t - minutes * 60
+        return this.myPadStart(minutes, 2) + ":" + this.myPadStart(second, 2)
+    },
+    // 节流
+    throttle(func, wait, type) {
+        if (type === 1) {
+            var previous = 0;
+        } else if (type === 2) {
+            var timeout;
+        }
+        return function () {
+            let context = this;
+            let args = arguments;
+            if (type === 1) {
+                let now = Date.now();
+
+                if (now - previous > wait) {
+                    func.apply(context, args);
+                    previous = now;
+                }
+            } else if (type === 2) {
+                if (!timeout) {
+                    timeout = setTimeout(() => {
+                        timeout = null;
+                        func.apply(context, args)
+                    }, wait)
+                }
+            }
+        }
     }
 }
